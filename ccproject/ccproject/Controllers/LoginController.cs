cf95745f27bbc702703233ff1847f1a2d802ab3b
greencometkhiz <khizratariqsyeda@gmail.com>
2023-06-19 12:43:38 -0700

    re

---
+++ ccproject/ccproject/Controllers/LoginController.cs
@@ -0,0 +1,67 @@
+﻿using ccproject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ccproject.Controllers
+{
+    public class LoginController : Controller
+    {
+        ccobjEntities2 db = new ccobjEntities2();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(user u)
+        {
+            var user = db.users.Where(model => model.username == u.username && model.password == u.password).FirstOrDefault();
+            if (user != null)
+            {
+                Session["UserId"] = u.Id.ToString();
+                Session["Username"] = u.username.ToString();
+                TempData["LoginSuccessMessage"] = "<script>alert('Login successfull!')</script>";
+                if (u.username == "Admin" && u.password == "123")
+                {
+                    return RedirectToAction("Admindashboard", "Dashboards");
+                }
+                else
+                {
+                    return RedirectToAction("Userdashboard", "Dashboards");
+                }
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "<script>alert('Username or Password is incorrect!')</script>";
+                return View();
+            }
+
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(user u)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.users.Add(u);
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    ViewBag.InsertMessage = "<script>alert('Registered Successfully')</script>";
+                    ModelState.Clear();
+                }
+                else
+                {
+                    ViewBag.InsertMessage = "<script>alert('Registeration failed')</script>";
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ ccproject/ccproject/Controllers/UServiceMvcController.cs
@@ -0,0 +1,53 @@
+﻿using ccproject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ccproject.Controllers
+{
+    public class UServiceMvcController : Controller
+    {
+        HttpClient client = new HttpClient();
+
+        public ActionResult Index()
+
+        {
+
+            List<Service> emp_list = new List<Service>();
+
+            client.BaseAddress = new Uri("https://localhost:44344/api/UServiceApi");
+            var response = client.GetAsync("UServiceApi");
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<Service>>();
+                display.Wait();
+                emp_list = display.Result;
+            }
+            return View(emp_list);
+        }
+       
+       
+        public ActionResult Details(int ID)
+        {
+            Service a = null;
+            client.BaseAddress = new Uri("https://localhost:44344/api/UServiceApi");
+            var response = client.GetAsync("UServiceApi?ID=" + ID.ToString());
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Service>();
+                display.Wait();
+                a = display.Result;
+            }
+
+            return View(a);
+        }
+       
+    }
+}
